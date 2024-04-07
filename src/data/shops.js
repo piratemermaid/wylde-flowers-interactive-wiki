2431c5eeb75ddb2c25e2cf09e63b74fd025ec810
Ariel <thepiratemermaid4@gmail.com>
2024-04-07 11:02:34 -0600

    Add shop data

---
+++ src/data/shops.js
@@ -0,0 +1,87 @@
+export const shops = [
+  {
+    name: 'Blacksmith',
+    owner: 'Natalia',
+    closures: ['TU', 'SU'],
+  },
+  {
+    name: 'Jeweler',
+    owner: 'Francis',
+    closures: ['M', 'TU', 'SU'],
+  },
+  {
+    name: 'Baker',
+    owner: 'Angus',
+    closures: ['W', 'SU'],
+  },
+  {
+    name: 'Flower',
+    owner: 'Violet',
+    closures: ['M', 'SU'],
+  },
+  {
+    name: 'Carpenter',
+    owner: 'Parker',
+    closures: ['SA', 'SA'],
+  },
+  {
+    name: 'General Store',
+    owner: 'Lina',
+    closures: ['TH', 'SU'],
+  },
+  {
+    name: "Kai's Treasures",
+    owner: 'Kai',
+    closures: ['M', 'W', 'TH'],
+  },
+  {
+    name: 'Fish',
+    owner: 'Bruno',
+    closures: ['M', 'F', 'SU'],
+  },
+  {
+    name: 'Diner',
+    owner: 'Sophia',
+    closures: ['M', 'SA'],
+  },
+  {
+    name: 'Doctor',
+    owner: 'Amira',
+    closures: ['M', 'F', 'SU'],
+  },
+  {
+    name: 'Ranch',
+    owner: 'Marty',
+    closures: ['TH', 'F', 'SA'],
+  },
+  {
+    name: 'Consciousness Center',
+    owner: 'Cameron',
+    closures: ['W', 'F'],
+  },
+  {
+    name: 'Bar',
+    owner: 'Damon',
+    closures: ['TU', 'SU'],
+  },
+  {
+    name: "Aryel's Shop",
+    owner: 'Aryel',
+    closures: ['M', 'TU'],
+  },
+  {
+    name: "Peri's Apothecary",
+    owner: 'Peri',
+    closures: ['M', 'TU'],
+  },
+  {
+    name: "Westley's Shop",
+    owner: 'Westley',
+    closures: ['TU', 'W'],
+  },
+  {
+    name: "Zephyr's Shop",
+    owner: 'Zephyr',
+    closures: ['M', 'TU'],
+  },
+];
