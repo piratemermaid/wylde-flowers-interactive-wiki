4ec26cb711aa5f1fa5d3920adeb6a46ceb8d1868
Ariel <thepiratemermaid4@gmail.com>
2024-04-07 12:28:57 -0600

    add ability to favorite shops

--- src/components/ShopSchedulesByDay.jsx
+++ src/components/ShopSchedulesByDay.jsx
@@ -1,13 +1,20 @@
 import {
   GridItem,
   SimpleGrid,
+  Text,
   VStack,
   useColorModeValue,
 } from '@chakra-ui/react';
+import { IconButton } from '@chakra-ui/react';
+import { StarIcon } from '@chakra-ui/icons';
 
+import { useStore } from '../store';
 import { shopSchedulesByDay } from '../data/shops';
 
 export default function ShopSchedulesByDay({ selectedDay }) {
+  const favoriteShops = useStore((state) => state.favoriteShops);
+  const toggleFavoriteShop = useStore((state) => state.toggleFavoriteShop);
+
   const shops = shopSchedulesByDay[selectedDay];
 
   const openShopColor = useColorModeValue('green.400', 'green.500');
@@ -23,10 +30,26 @@ export default function ShopSchedulesByDay({ selectedDay }) {
               columns={2}
               sx={{
                 backgroundColor: open ? openShopColor : closedShopColor,
-                width: 60,
+                width: 80,
               }}
+              fontWeight={favoriteShops.includes(name) ? 400 : 300}
             >
-              <GridItem sx={{ textAlign: 'left', ml: 2 }}>{name}</GridItem>
+              <GridItem sx={{ textAlign: 'left' }}>
+                <Text>
+                  <IconButton
+                    icon={<StarIcon />}
+                    aria-label="Favorite shop"
+                    color={
+                      favoriteShops.includes(name)
+                        ? 'secondary.700'
+                        : 'gray.300'
+                    }
+                    sx={{ position: 'relative', bottom: 0.5, height: 3 }}
+                    onClick={() => toggleFavoriteShop(name)}
+                  />
+                  {name}
+                </Text>
+              </GridItem>
               <GridItem sx={{ textAlign: 'right', mr: 2 }}>{owner}</GridItem>
             </SimpleGrid>
           );
--- src/store.js
+++ src/store.js
@@ -3,10 +3,24 @@ import { persist } from 'zustand/middleware';
 
 export const useStore = create(
   persist(
-    (set) => ({
+    (set, get) => ({
       notes: '',
       setNotes: (newNotes) => {
         return set({ notes: newNotes });
+      },
+
+      favoriteShops: [],
+      toggleFavoriteShop: (shop) => {
+        const favoriteShops = get().favoriteShops;
+        let newFavoriteShops;
+
+        if (favoriteShops.includes(shop)) {
+          newFavoriteShops = favoriteShops.filter((fshop) => fshop !== shop);
+        } else {
+          newFavoriteShops = [...favoriteShops, shop];
+        }
+
+        return set({ favoriteShops: newFavoriteShops });
       },
     }),
     {
